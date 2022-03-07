94944814c07f3bece04ebf1a88c2e740514e25f7
Andrei Formiga <archimedes_siracusa@hotmail.com>
2022-03-07 13:03:05 -0300

    Solution for 2-2: documenting a module

---
+++ 2-2/geom.erl
@@ -0,0 +1,15 @@
+%% @author J D Eisenberg <jdavid.eisenberg@gmail.com>
+%% @doc Functions for calculating areas of geometric shapes.
+%% @copyright 2013 J D Eisenberg
+%% @version 0.1
+
+-module(geom).
+-export([area/2]).
+
+%% @doc Calculates the area of a rectangle, given the length L and width W.
+%% Returns the product of its arguments.
+
+-spec(area(number(), number()) -> number()).
+
+area(L, W) ->
+    L * W.
